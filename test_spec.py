8fab83698261dab8416c07d6887259c233e08ed0
Dominik Moritz <domoritz@gmail.com>
2017-08-17 11:48:25 -0700

    Fix test

--- test_spec.py
+++ test_spec.py
@@ -3,4 +3,4 @@ from spec import Field
 class TestField():
     def test_to_asp(self):
         f = Field("foo", "number", 100)
-        assert f.to_asp() == "fieldtype(foo,number).\ncardinality(foo,100)."
+        assert f.to_asp() == "fieldtype(foo,number).\ncardinality(foo,100).\n"
