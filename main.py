6539d7e03463538d8e2a0ff718be0545c24727e6
Dominik Moritz <domoritz@gmail.com>
2017-08-16 18:14:57 -0700

    Use real logger and support stdout and in

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
-from spec import *
+import os
+import sys
 import json
 import argparse
 import subprocess
-
+import logging
 from pprint import pprint
+
+from spec import *
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 CONFIG = {
     # the clingcon bin file
@@ -22,22 +28,20 @@ if not os.path.exists(CONFIG["tmp_dir"]):
     os.makedirs(CONFIG["tmp_dir"])
 
 
-def main(partial_vl_spec):
+def main(partial_vl_spec, out):
     """ Given a partial vegalite spec, recommand a completion of the spec
     """
-    tmp_asp_file = os.path.join(CONFIG["tmp_dir"], os.path.basename(partial_vl_spec).split(".")[0] + ".lp")
+    tmp_asp_file = os.path.join(CONFIG["tmp_dir"], os.path.basename(partial_vl_spec.name).split(".")[0] + ".lp")
 
     # load a task from a spec provided by the user
     task = Task.load_from_vl_json(partial_vl_spec)
 
     with open(tmp_asp_file, "w") as f:
-        print(f"[OK] Temp asp specification written into: {tmp_asp_file} .")
+        logger.info(f"Temp asp specification written into: {tmp_asp_file}.")
         f.write(task.to_asp())
 
     r = subprocess.run([CONFIG["clingo"], CONFIG["vega_lite_lp"], tmp_asp_file, "--outf=2"],
                        stdout=subprocess.PIPE, stderr=None)
-
-    print("[Solver Output]")
 
     json_result = json.loads(r.stdout.decode("utf-8"))
 
@@ -48,18 +52,28 @@ def main(partial_vl_spec):
     query = Query.parse_from_asp_result(raw_str_list)
     new_task = Task(task.data, query)
 
-    print(json.dumps(new_task.to_vegalite_obj()))
-
+    print(json.dumps(new_task.to_vegalite_obj()), file=out)
+    logger.info(f"Wrote Vega-Lite spec to {out.name}.")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Visualization recommendation system.",
+        epilog="There is a moment in every dawn when light floats, there is the possibility of magic. Creation holds its breath.")
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("vegalite_spec", nargs="?", default=os.path.join("examples", "ab.vl.json"),
-                        help="The partial vegalite spec for completion")
+    parser.add_argument("query", nargs="?", type=argparse.FileType('r'), default=sys.stdin,
+                        help="The CompassQL query (partial Vega-Lite spec).")
+    parser.add_argument("--out", nargs="?", type=argparse.FileType('w'), default=sys.stdout,
+                        help="Where to output the Vega-Lite spec.")
     args = parser.parse_args()
 
-    print(f"[OK] Processing partial vis spec: {args.vegalite_spec} ...")
+    logger.info(f"Processing partial vis spec: {args.query.name} ...")
 
-    main(args.vegalite_spec)
+    main(args.query, args.out)
 
-    print(f"[OK] Complete task.")
+    # close open files
+    if args.query is not sys.stdin:
+        args.query.close()
+
+    if args.out is not sys.stdout:
+        args.out.close()
+
+    logger.info(f"Complete task.")
