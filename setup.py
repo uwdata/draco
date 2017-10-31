e1b621ad6e3c9376028c90b8561bb52e277b2e53
Dominik Moritz <domoritz@gmail.com>
2017-10-30 19:00:39 -0700

    License

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ from distutils.core import setup
 setup(name='Draco',
       version='0.1',
       description='Visualization recommendation using consraints',
-      author='',
-      author_email='',
-      url='',
+      author='Dominik Moritz, Chenglong Wang',
+      author_email='domoritz@cs.washington.edu, clwang@cs.washington.edu',
+      license='BSD-3',
+      url='https://github.com/domoritz/draco',
       packages=['draco'],
      )
