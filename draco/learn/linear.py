c9a89427a7bd93fcd910234113b3a516a6321f1d
Dominik Moritz <domoritz@gmail.com>
2018-02-06 18:03:22 -0800

    Plot with seaborn

--- draco/learn/linear.py
+++ draco/learn/linear.py
@@ -1,34 +1,30 @@
-import matplotlib.pyplot as plt
+from typing import Tuple
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import sklearn
-import sklearn.preprocessing as preproc
+import seaborn as sns
 from sklearn import svm
 from sklearn.decomposition import PCA
 
 from draco.learn import data_util
-from draco.learn.helper import count_violations, current_weights
-from draco.spec import Data, Field
 
-def prepare_data(data):
 
+def prepare_data(data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
     N = len(data)
-    num_features = len(data["positive"].columns)
 
-    X = np.zeros((2 * N, num_features))
-    y = np.zeros(2 * N)
+    X = pd.DataFrame(index=range(2*N), columns=data.positive.columns)
+    y = pd.DataFrame(np.zeros(2 * N), columns=['label'])
 
     for i in range(N):
+        x_pos = data.positive.iloc[i].values
+        x_neg = data.negative.iloc[i].values
 
-        x_pos = data["positive"].iloc[i].values
-        x_neg = data["negative"].iloc[i].values
+        X.iloc[i] = x_pos - x_neg
+        y.iloc[i] = 1
 
-        X[i] = x_pos - x_neg
-        y[i] = 1
-
-        X[i + N] = x_neg - x_pos
-        y[i + N] = 0
+        X.iloc[i + N] = x_neg - x_pos
+        y.iloc[i + N] = 0
 
     return X, y
 
@@ -37,6 +33,7 @@ def learn_weights(X_train, y_train, X_dev, y_dev):
 
     #clf = sklearn.linear_model.LogisticRegression()
     clf = svm.LinearSVC(C=.1)
+
     clf.fit(X_train, y_train)
     coef = clf.coef_.ravel() / np.linalg.norm(clf.coef_)
     #print(coef)
@@ -47,24 +44,14 @@ def learn_weights(X_train, y_train, X_dev, y_dev):
 
 
 def plot_data(X, y):
-
-    print(len(list(set([tuple([int(t) for t in x]) for x in X]))))
-
     pca = PCA(n_components=2)
-    X = pca.fit_transform(X)
+    fitted = pd.DataFrame(pca.fit_transform(X))
 
-    labels = list(set(y))
+    fitted[2] = y
+    fitted.columns = ['x1', 'x2', 'label']
 
-    class_label = [('^', 'r'), ('o', 'b'), ('*', 'b'), ('*', 'y'), ('*', 'b')]
-
-    for v in labels:
-        x1 = [X[i][0] for i in range(len(X)) if y[i] == v]
-        x2 = [X[i][1] for i in range(len(X)) if y[i] == v]
-
-        label = class_label[labels.index(v)]
-        plt.scatter(x1, x2, marker=label[0], c=label[1])
-
-    return plt
+    sns.lmplot(data=fitted, x='x1', y='x2', fit_reg=False, hue='label')
+    plt.show()
 
 if __name__ == '__main__':
 
@@ -76,9 +63,5 @@ if __name__ == '__main__':
     X_dev, y_dev = prepare_data(dev_data)
 
     learn_weights(X_train, y_train, X_dev, y_dev)
-    
-    plt = plot_data(*prepare_data(train_data))    
-    #plt = plot_data(*prepare_data(dev_data))
-    plt.show()
 
-
+    plot_data(X_train, y_train)
