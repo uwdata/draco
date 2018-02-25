1b59581048b749ac142e8ca90136f97d5d3745b2
Dominik Moritz <domoritz@gmail.com>
2018-02-25 13:33:51 -0800

    Add requirements for server, support calling from anywhere

--- draco-tools/server/labeler.py
+++ draco-tools/server/labeler.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import json
+import os
 
 
 app = Flask(__name__)
@@ -8,15 +9,15 @@ CORS(app)
 
 @app.route('/pair', methods=['GET'])
 def get_pair():
-  with open('example.json') as data:
-    return jsonify(json.load(data))
+    with open(os.path.join(os.path.dirname(__file__), 'example.json')) as data:
+        return jsonify(json.load(data))
 
 @app.route('/pair', methods=['POST'])
 def choose():
-  if not request or not 'id' in request.json or not 'label' in request.json:
-    abort(400)
-  print(request.json)
-  return 'success'
+    if not request or not 'id' in request.json or not 'label' in request.json:
+        abort(400)
+    print(request.json)
+    return 'success'
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
