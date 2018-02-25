a06b9b74cede384005b71e6db5a2a9cd7d394b33
haldenl <haldenl@cs.washington.edu>
2018-02-25 06:23:38 -0800

    dummy server for labeling

---
+++ draco-tools/server/labeler.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/pair', methods=['GET'])
+def get_pair():
+  with open('example.json') as data:
+    return jsonify(json.load(data))
+
+@app.route('/pair', methods=['POST'])
+def choose():
+  if not request or not 'id' in request.json or not 'label' in request.json:
+    abort(400)
+  print(request.json)
+  return 'success'
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
