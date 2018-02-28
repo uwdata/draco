d6e5c60d945ee192074d20a6abc453406ec47aab
Dominik Moritz <domoritz@gmail.com>
2018-02-27 18:03:01 -0800

    Set bug file via URL

--- draco-tools/src/bugviewer/js/components/BugViewer.js
+++ draco-tools/src/bugviewer/js/components/BugViewer.js
@@ -3,39 +3,57 @@ import 'bugviewer/scss/BugViewer.css';
 
 import Visualization from 'shared/js/components/Visualization';
 
-const BUGS = require('bugviewer/bugs.json');
-
 class BugViewer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bugs: undefined,
+    };
+  }
+
+  componentDidMount() {
+    fetch((new URL(window.location.href)).searchParams.get("data") || 'bugs.json')
+      .then(response => response.json())
+      .then(data => this.setState({ bugs: data }));
+  }
+
   render() {
+    const BUGS = this.state.bugs;
+
+    if (!BUGS) {
+      return <div>loading...</div>
+    }
+
+    const headers = BUGS.headers;
+
     const bugs = [];
-    for (let i = 0; i < BUGS.length; i++) {
-      const bug = BUGS[i];
+    for (let i = 0; i < 5; i++) {
+      const bug = BUGS.specs[i];
+
+      const properties = Object.keys(bug.properties || {}).map((p, i) => <p key={i}><strong>{p}: </strong>{bug.properties[p]}</p>)
 
       bugs.push(
         <div className="bug" key={i}>
           <div className="visualizations">
-            <div>
-              <span className="label">
-                Negative<br/><small>but was predicted as better</small>
-              </span>
-              <Visualization vlSpec={bug.negative}/>
-            </div>
-            <div>
-              <span className="label">
-                Positive<br/><small>but was predicted as worse</small>
-              </span>
-              <Visualization vlSpec={bug.positive}/>
-            </div>
+            <Visualization vlSpec={bug.negative}/>
+            <Visualization vlSpec={bug.positive}/>
           </div>
-          <p>
-            Confidence: {bug.confidence}
-          </p>
+          {properties}
         </div>
       );
     }
 
     return (
       <div className="BugViewer">
+        <div className="header">
+          <div className="label">
+            {headers.first.title}<br/><small>{headers.first.subtitle}</small>
+          </div>
+          <div className="label">
+            {headers.second.title}<br/><small>{headers.second.subtitle}</small>
+          </div>
+        </div>
         {bugs}
       </div>
     );
