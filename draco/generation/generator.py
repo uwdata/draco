6999c589e585d9c7a289974337127d828431a1a6
haldenl <haldenl@cs.washington.edu>
2018-03-02 11:57:04 -0800

    update weights for marks

--- draco/generation/generator.py
+++ draco/generation/generator.py
@@ -9,122 +9,138 @@ from draco.generation.model import Model
 from draco.generation.helper import is_valid
 from draco.spec import Task, Data, Query, Field
 
-INTERACTIONS_PATH = os.path.join(os.path.dirname(__file__), 'interactions.json')
-DISTRIBUTIONS_PATH = os.path.join(os.path.dirname(__file__), 'distributions.json')
+INTERACTIONS_PATH = os.path.join(
+    os.path.dirname(__file__), 'interactions.json')
+DISTRIBUTIONS_PATH = os.path.join(
+    os.path.dirname(__file__), 'distributions.json')
 DEFINITIONS_PATH = os.path.join(os.path.dirname(__file__), 'definitions.json')
-DUMMY_SCHEMA_PATH = os.path.join(os.path.dirname(__file__), 'dummy_schema.json')
+DUMMY_SCHEMA_PATH = os.path.join(
+    os.path.dirname(__file__), 'dummy_schema.json')
 
 NUM_GROUPS = 20
 NUM_TRIES = 100
 
+
 def main():
-  interactions = load_json(INTERACTIONS_PATH)
-  distributions = load_json(DISTRIBUTIONS_PATH)
-  definitions = load_json(DEFINITIONS_PATH)
-  dummy_schema = load_json(DUMMY_SCHEMA_PATH)
+    interactions = load_json(INTERACTIONS_PATH)
+    distributions = load_json(DISTRIBUTIONS_PATH)
+    definitions = load_json(DEFINITIONS_PATH)
+    dummy_schema = load_json(DUMMY_SCHEMA_PATH)
 
-  model = Model(distributions, definitions['topLevelProps'], definitions['encodingProps'])
+    model = Model(
+        distributions, definitions['topLevelProps'], definitions['encodingProps'])
 
-  dummy_fields = [Field(x['name'], x['type']) for x in dummy_schema]
-  dummy_data = Data(dummy_fields)
+    dummy_fields = [Field(x['name'], x['type']) for x in dummy_schema]
+    dummy_data = Data(dummy_fields)
 
-  interaction_groups = []
+    interaction_groups = []
 
-  count = 0
-  pairs = 0
-  for i in tqdm(range(len(interactions))):
-    interaction = interactions[i]
-    if (interaction['include']):
-      groups = []
-      
-      for j in tqdm(range(NUM_GROUPS)):
+    count = 0
+    pairs = 0
+    for i in tqdm(range(len(interactions))):
+        interaction = interactions[i]
+        if (interaction['include']):
+            groups = []
 
-        for d in tqdm(range(1, 4)):
-          specs = generate_interaction(model, dummy_data, interaction, d)
+            for j in tqdm(range(NUM_GROUPS)):
 
-          for tries in tqdm(range(NUM_TRIES)):
-            specs = generate_interaction(model, dummy_data, interaction, d)
-            if (len(specs) >= 2):
-              break
+                for d in tqdm(range(1, 4)):
+                    specs = generate_interaction(
+                        model, dummy_data, interaction, d)
 
-          groups.append(specs)
-          count += len(specs)
-          pairs += math.factorial(len(specs)) // 2
-      
-      out = { 'interaction': interaction['name'], 'groups': groups }
-      with open('out/{0}.json'.format(interaction['name']), 'w') as outfile:
-        json.dump(out, outfile, indent=2)
+                    for tries in tqdm(range(NUM_TRIES)):
+                        specs = generate_interaction(
+                            model, dummy_data, interaction, d)
+                        if (len(specs) >= 2):
+                            break
 
-  print('{0} specs with {1} pairs for {2} interactions'.format(count, pairs, len(interactions)))
+                    groups.append(specs)
+                    count += len(specs)
+                    pairs += math.factorial(len(specs)) // 2
+
+            out = {'interaction': interaction['name'], 'groups': groups}
+            with open('out/{0}.json'.format(interaction['name']), 'w') as outfile:
+                json.dump(out, outfile, indent=2)
+
+    print('{0} specs with {1} pairs for {2} interactions'.format(
+        count, pairs, len(interactions)))
+
 
 def generate_interaction(model, dummy_data, interaction, dimensions):
-  props = interaction['props'].copy()
-  base_spec = model.generate_spec(dimensions)
+    props = interaction['props'].copy()
+    base_spec = model.generate_spec(dimensions)
 
-  specs = []
-  mutate_spec(model, dummy_data, deepcopy(base_spec), props, specs)
-  return specs
+    specs = []
+    mutate_spec(model, dummy_data, deepcopy(base_spec), props, specs)
+    return specs
+
 
 def mutate_spec(model, dummy_data, base_spec, props, specs):
-  if (not props):
-    spec = deepcopy(base_spec)
-    populate_field_names(spec)
-  
-    to_vegalite(spec)
-    query = Query.from_vegalite(spec)
-    if (is_valid(Task(dummy_data, query))):
-      specs.append(spec)
-  else:
-    prop_to_mutate = props.pop(0)
+    if (not props):
+        spec = deepcopy(base_spec)
+        populate_field_names(spec)
 
-    is_top_level_prop = prop_to_mutate in model.get_top_level_props()
-    used_enums = Model.get_enums_used_for_prop(model, base_spec, prop_to_mutate)
+        to_vegalite(spec)
+        query = Query.from_vegalite(spec)
+        if (is_valid(Task(dummy_data, query))):
+            specs.append(spec)
+    else:
+        prop_to_mutate = props.pop(0)
 
-    for enum in model.get_enums(prop_to_mutate):
-      if (is_top_level_prop):
-        base_spec[prop_to_mutate] = enum
-        mutate_spec(model, dummy_data, base_spec, props, specs)
-      elif (not enum in used_enums):
-        encodings = base_spec['encodings']
-        index = random.randint(0, len(encodings) - 1)
+        is_top_level_prop = prop_to_mutate in model.get_top_level_props()
+        used_enums = Model.get_enums_used_for_prop(
+            model, base_spec, prop_to_mutate)
 
-        before = None
-        if (prop_to_mutate in encodings[index]):
-          before = encodings[index][prop_to_mutate]
+        for enum in model.get_enums(prop_to_mutate):
+            if (is_top_level_prop):
+                base_spec[prop_to_mutate] = enum
+                mutate_spec(model, dummy_data, base_spec, props, specs)
+            elif (not enum in used_enums):
+                encodings = base_spec['encodings']
+                index = random.randint(0, len(encodings) - 1)
 
-        encodings[index][prop_to_mutate] = Model.build_value_from_enum(prop_to_mutate, enum)
-        mutate_spec(model, dummy_data, base_spec, props, specs)
+                before = None
+                if (prop_to_mutate in encodings[index]):
+                    before = encodings[index][prop_to_mutate]
 
-        if (not before is None):
-          encodings[index][prop_to_mutate] = before
-  
+                encodings[index][prop_to_mutate] = Model.build_value_from_enum(
+                    prop_to_mutate, enum)
+                mutate_spec(model, dummy_data, base_spec, props, specs)
+
+                if (not before is None):
+                    encodings[index][prop_to_mutate] = before
+
+
 def to_vegalite(spec):
-  old_encodings = spec['encodings']
-  del spec['encodings']
-  spec['encoding'] = {}
-  for enc in old_encodings:
-    channel = enc['channel']
-    del enc['channel']
-    spec['encoding'][channel] = enc
+    old_encodings = spec['encodings']
+    del spec['encodings']
+    spec['encoding'] = {}
+    for enc in old_encodings:
+        channel = enc['channel']
+        del enc['channel']
+        spec['encoding'][channel] = enc
 
-  return
+    return
+
 
 def populate_field_names(spec):
-  counts = {
-    'n': 1, 'o': 1, 'q': 1, 't': 1
-  }
+    counts = {
+        'n': 1, 'o': 1, 'q': 1, 't': 1
+    }
 
-  encodings = spec['encodings']
-  for enc in encodings:
-    field_type = enc['type'][:1]
-    field_name = field_type + str(counts[field_type])
-    counts[field_type] += 1
+    encodings = spec['encodings']
+    for enc in encodings:
+        field_type = enc['type'][:1]
+        field_name = field_type + str(counts[field_type])
+        counts[field_type] += 1
 
-    enc['field'] = field_name
+        enc['field'] = field_name
+
 
 def load_json(file_path):
-  with open(file_path) as data:
-    return json.load(data)
+    with open(file_path) as data:
+        return json.load(data)
+
 
 if __name__ == '__main__':
-  main()
+    main()
--- draco/generation/model.py
+++ draco/generation/model.py
@@ -4,154 +4,157 @@ from copy import deepcopy
 
 from prop_objects import PropObjects
 
+
 class Model:
-  SPECIAL_ENUMS = {
-    'bin': PropObjects.get_bin,
-    'scale': PropObjects.get_scale,
-  }
+    SPECIAL_ENUMS = {
+        'bin': PropObjects.get_bin,
+        'scale': PropObjects.get_scale,
+    }
 
-  UNPACK_SPECIAL_ENUM = {
-    'bin': PropObjects.unpack_bin,
-    'scale': PropObjects.unpack_scale
-  }
+    UNPACK_SPECIAL_ENUM = {
+        'bin': PropObjects.unpack_bin,
+        'scale': PropObjects.unpack_scale
+    }
 
-  UNIQUE_ENCODING_PROPS = set(['channel'])
+    UNIQUE_ENCODING_PROPS = set(['channel'])
 
-  def __init__(self, distributions, top_level_props, encoding_props):
-    self.distributions = distributions
-    self.top_level_props = set(top_level_props)
-    self.encoding_props = set(encoding_props)
+    def __init__(self, distributions, top_level_props, encoding_props):
+        self.distributions = distributions
+        self.top_level_props = set(top_level_props)
+        self.encoding_props = set(encoding_props)
 
-    self.enums = {}
-    self.probs = {}
-    self.enum_probs = {}
+        self.enums = {}
+        self.probs = {}
+        self.enum_probs = {}
 
-    for spec in distributions:
-      self.enums[spec] = [x['name'] for x in distributions[spec]['values']]
-      self.probs[spec] = [x['probability'] for x in distributions[spec]['values']]
+        for spec in distributions:
+            self.enums[spec] = [x['name']
+                                for x in distributions[spec]['values']]
+            self.probs[spec] = [x['probability']
+                                for x in distributions[spec]['values']]
 
-    for prop in self.enums:
-      self.enum_probs[prop] = {}
-      enums = self.enums[prop]
-      probs = self.probs[prop]
+        for prop in self.enums:
+            self.enum_probs[prop] = {}
+            enums = self.enums[prop]
+            probs = self.probs[prop]
 
-      for i in range(len(enums)):
-        self.enum_probs[prop][enums[i]] = probs[i]
+            for i in range(len(enums)):
+                self.enum_probs[prop][enums[i]] = probs[i]
 
-    return
+        return
 
-  def ready(self):
-    """
-    Prepares this to generate a spec
-    """
-    self.curr_enums = deepcopy(self.enums)
-    self.curr_probs = deepcopy(self.probs)
+    def ready(self):
+        """
+        Prepares this to generate a spec
+        """
+        self.curr_enums = deepcopy(self.enums)
+        self.curr_probs = deepcopy(self.probs)
 
-  def generate_spec(self, n_dimensions):
-    """
-    Returns a spec, randomizing props.
+    def generate_spec(self, n_dimensions):
+        """
+        Returns a spec, randomizing props.
 
-    n_dimensions -- the number of encodings to generate
-    """
-    self.ready()
-    spec = { 'encodings': [] }
-    
-    for prop in self.top_level_props:
-      if (self.include(prop)):
-        spec[prop] = self.sample_enum(prop)
-    
-    for d in range(n_dimensions):
-      enc = self.generate_enc()
-      spec['encodings'].append(enc)
+        n_dimensions -- the number of encodings to generate
+        """
+        self.ready()
+        spec = {'encodings': []}
 
-    return spec
+        for prop in self.top_level_props:
+            if (self.include(prop)):
+                spec[prop] = self.sample_enum(prop)
 
-  def generate_enc(self):
-    """
-    Returns an encoding, randomizing props.
-    """
-    enc = {}
+        for d in range(n_dimensions):
+            enc = self.generate_enc()
+            spec['encodings'].append(enc)
 
-    for prop in self.encoding_props:
-      if (self.include(prop)):
-        enc[prop] = self.sample_prop(prop)
+        return spec
 
-    return enc
+    def generate_enc(self):
+        """
+        Returns an encoding, randomizing props.
+        """
+        enc = {}
 
-  def get_enum_probabilities(self, prop):
-    return self.enum_probs[prop]
+        for prop in self.encoding_props:
+            if (self.include(prop)):
+                enc[prop] = self.sample_prop(prop)
 
-  def get_enums(self, prop):
-    return self.enums[prop]
+        return enc
 
-  def get_top_level_props(self):
-    return self.top_level_props
+    def get_enum_probabilities(self, prop):
+        return self.enum_probs[prop]
 
-  def get_encoding_props(self):
-    return self.encoding_props
+    def get_enums(self, prop):
+        return self.enums[prop]
 
-  def include(self, prop):
-    """
-    Decides randomly from `self.distributions` whether or not
-    the given spec should be included
-    """
-    prob = self.distributions[prop]['probability']
-    return random.random() < prob
+    def get_top_level_props(self):
+        return self.top_level_props
 
-  def sample_prop(self, prop):
-    enum = self.sample_enum(prop)
-    if (prop in Model.SPECIAL_ENUMS):
-      return Model.SPECIAL_ENUMS[prop](enum)
+    def get_encoding_props(self):
+        return self.encoding_props
 
-    return enum
+    def include(self, prop):
+        """
+        Decides randomly from `self.distributions` whether or not
+        the given spec should be included
+        """
+        prob = self.distributions[prop]['probability']
+        return random.random() < prob
 
-  def sample_enum(self, prop):
-    """
-    Returns a random value for the given prop.
-    
-    Params:
-    distributions -- {object} see `distributions.json`
-    spec -- {string} e.g. `mark`, `channel`, etc.
-    """
-    enums = self.curr_enums[prop]
-    probs = self.curr_probs[prop]
+    def sample_prop(self, prop):
+        enum = self.sample_enum(prop)
+        if (prop in Model.SPECIAL_ENUMS):
+            return Model.SPECIAL_ENUMS[prop](enum)
 
-    cumulative = np.cumsum(probs)
+        return enum
 
-    choice = random.uniform(0, cumulative[-1])
-    index = np.searchsorted(cumulative, choice)
-    if (index == len(cumulative)):
-      # in case choice rests exactly on the upper bound
-      index -= 1
+    def sample_enum(self, prop):
+        """
+        Returns a random value for the given prop.
 
-    result = enums[index]
+        Params:
+        distributions -- {object} see `distributions.json`
+        spec -- {string} e.g. `mark`, `channel`, etc.
+        """
+        enums = self.curr_enums[prop]
+        probs = self.curr_probs[prop]
 
-    if (prop in Model.UNIQUE_ENCODING_PROPS):
-      enums.pop(index)
-      probs.pop(index)
+        cumulative = np.cumsum(probs)
 
-    return result
+        choice = random.uniform(0, cumulative[-1])
+        index = np.searchsorted(cumulative, choice)
+        if (index == len(cumulative)):
+            # in case choice rests exactly on the upper bound
+            index -= 1
 
-  @staticmethod
-  def get_enums_used_for_prop(model, spec, prop):
-    used = set()
-    if (prop in model.get_top_level_props() and prop in spec):
-      used.add(spec[prop])
-    elif (prop in model.get_encoding_props()):
-      encodings = spec['encodings']
-      
-      for enc in encodings:
-        if (prop in enc):
-          if (prop in Model.UNPACK_SPECIAL_ENUM):
-            used.add(Model.UNPACK_SPECIAL_ENUM[prop](enc[prop]))
-          else:
-            used.add(enc[prop])
-    
-    return used
+        result = enums[index]
 
-  @staticmethod
-  def build_value_from_enum(prop, enum):
-    if (prop in Model.SPECIAL_ENUMS):
-      return Model.SPECIAL_ENUMS[prop](enum)
-    else:
-      return enum
+        if (prop in Model.UNIQUE_ENCODING_PROPS):
+            enums.pop(index)
+            probs.pop(index)
+
+        return result
+
+    @staticmethod
+    def get_enums_used_for_prop(model, spec, prop):
+        used = set()
+        if (prop in model.get_top_level_props() and prop in spec):
+            used.add(spec[prop])
+        elif (prop in model.get_encoding_props()):
+            encodings = spec['encodings']
+
+            for enc in encodings:
+                if (prop in enc):
+                    if (prop in Model.UNPACK_SPECIAL_ENUM):
+                        used.add(Model.UNPACK_SPECIAL_ENUM[prop](enc[prop]))
+                    else:
+                        used.add(enc[prop])
+
+        return used
+
+    @staticmethod
+    def build_value_from_enum(prop, enum):
+        if (prop in Model.SPECIAL_ENUMS):
+            return Model.SPECIAL_ENUMS[prop](enum)
+        else:
+            return enum
