e93aa9e0f79b4f8f8d712ac12d8234ac09c2a45a
haldenl <haldenl@cs.washington.edu>
2018-03-02 15:39:14 -0800

    clean up generator, run with cl arguments

--- draco/generation/generator.py
+++ draco/generation/generator.py
@@ -1,146 +1,88 @@
-import json
-import os
+from copy import deepcopy
 import random
 import math
-from tqdm import tqdm
-from copy import deepcopy
 
 from draco.generation.model import Model
 from draco.generation.helper import is_valid
 from draco.spec import Task, Data, Query, Field
 
-INTERACTIONS_PATH = os.path.join(
-    os.path.dirname(__file__), 'interactions.json')
-DISTRIBUTIONS_PATH = os.path.join(
-    os.path.dirname(__file__), 'distributions.json')
-DEFINITIONS_PATH = os.path.join(os.path.dirname(__file__), 'definitions.json')
-DUMMY_SCHEMA_PATH = os.path.join(
-    os.path.dirname(__file__), 'dummy_schema.json')
 
-NUM_GROUPS = 20
-NUM_TRIES = 100
+class Generator:
+    def __init__(self, distributions, definitions, data_schema):
+        top_level_props = definitions['topLevelProps']
+        encoding_props = definitions['encodingProps']
+        data_fields = [Field(x['name'], x['type']) for x in data_schema]
 
-
-def main():
-    interactions = load_json(INTERACTIONS_PATH)
-    distributions = load_json(DISTRIBUTIONS_PATH)
-    definitions = load_json(DEFINITIONS_PATH)
-    dummy_schema = load_json(DUMMY_SCHEMA_PATH)
-
-    model = Model(
-        distributions, definitions['topLevelProps'], definitions['encodingProps'])
-
-    dummy_fields = [Field(x['name'], x['type']) for x in dummy_schema]
-    dummy_data = Data(dummy_fields)
-
-    interaction_groups = []
-
-    count = 0
-    pairs = 0
-    for i in tqdm(range(len(interactions))):
-        interaction = interactions[i]
-        if (interaction['include']):
-            groups = []
-
-            for j in tqdm(range(NUM_GROUPS)):
-
-                for d in tqdm(range(1, 4)):
-                    specs = generate_interaction(
-                        model, dummy_data, interaction, d)
-
-                    for tries in tqdm(range(NUM_TRIES)):
-                        specs = generate_interaction(
-                            model, dummy_data, interaction, d)
-                        if (len(specs) >= 2):
-                            break
-
-                    groups.append(specs)
-                    count += len(specs)
-                    pairs += math.factorial(len(specs)) // 2
-
-            out = {'interaction': interaction['name'], 'groups': groups}
-            with open('out/{0}.json'.format(interaction['name']), 'w') as outfile:
-                json.dump(out, outfile, indent=2)
-
-    print('{0} specs with {1} pairs for {2} interactions'.format(
-        count, pairs, len(interactions)))
+        self.model = Model(distributions, top_level_props, encoding_props)
+        self.data = Data(data_fields)
 
 
-def generate_interaction(model, dummy_data, interaction, dimensions):
-    props = interaction['props'].copy()
-    base_spec = model.generate_spec(dimensions)
+    def generate_interaction(self, props, dimensions):
+        base_spec = self.model.generate_spec(dimensions)
 
-    specs = []
-    mutate_spec(model, dummy_data, deepcopy(base_spec), props, specs)
-    return specs
-
-
-def mutate_spec(model, dummy_data, base_spec, props, specs):
-    if (not props):
-        spec = deepcopy(base_spec)
-        populate_field_names(spec)
-
-        to_vegalite(spec)
-        query = Query.from_vegalite(spec)
-        if (is_valid(Task(dummy_data, query))):
-            specs.append(spec)
-    else:
-        prop_to_mutate = props.pop(0)
-
-        is_top_level_prop = prop_to_mutate in model.get_top_level_props()
-        used_enums = Model.get_enums_used_for_prop(
-            model, base_spec, prop_to_mutate)
-
-        for enum in model.get_enums(prop_to_mutate):
-            if (is_top_level_prop):
-                base_spec[prop_to_mutate] = enum
-                mutate_spec(model, dummy_data, base_spec, props, specs)
-            elif (not enum in used_enums):
-                encodings = base_spec['encodings']
-                index = random.randint(0, len(encodings) - 1)
-
-                before = None
-                if (prop_to_mutate in encodings[index]):
-                    before = encodings[index][prop_to_mutate]
-
-                encodings[index][prop_to_mutate] = Model.build_value_from_enum(
-                    prop_to_mutate, enum)
-                mutate_spec(model, dummy_data, base_spec, props, specs)
-
-                if (not before is None):
-                    encodings[index][prop_to_mutate] = before
+        specs = []
+        Generator.mutate_spec(self.model, self.data, deepcopy(base_spec), props.copy(), specs)
+        return specs
 
 
-def to_vegalite(spec):
-    old_encodings = spec['encodings']
-    del spec['encodings']
-    spec['encoding'] = {}
-    for enc in old_encodings:
-        channel = enc['channel']
-        del enc['channel']
-        spec['encoding'][channel] = enc
+    @staticmethod
+    def mutate_spec(model, dummy_data, base_spec, props, specs):
+        if (not props):
+            spec = deepcopy(base_spec)
+            Generator.populate_field_names(spec)
 
-    return
+            Generator.to_vegalite(spec)
+            query = Query.from_vegalite(spec)
+            if (is_valid(Task(dummy_data, query))):
+                specs.append(spec)
+        else:
+            prop_to_mutate = props.pop(0)
+
+            is_top_level_prop = prop_to_mutate in model.get_top_level_props()
+            used_enums = Model.get_enums_used_for_prop(
+                model, base_spec, prop_to_mutate)
+
+            for enum in model.get_enums(prop_to_mutate):
+                if (is_top_level_prop):
+                    base_spec[prop_to_mutate] = enum
+                    Generator.mutate_spec(model, dummy_data, base_spec, props, specs)
+                elif (not enum in used_enums):
+                    encodings = base_spec['encodings']
+                    index = random.randint(0, len(encodings) - 1)
+
+                    before = None
+                    if (prop_to_mutate in encodings[index]):
+                        before = encodings[index][prop_to_mutate]
+
+                    encodings[index][prop_to_mutate] = Model.build_value_from_enum(
+                        prop_to_mutate, enum)
+                    Generator.mutate_spec(model, dummy_data, base_spec, props, specs)
+
+                    if (not before is None):
+                        encodings[index][prop_to_mutate] = before
+
+    @staticmethod
+    def to_vegalite(spec):
+        old_encodings = spec['encodings']
+        del spec['encodings']
+        spec['encoding'] = {}
+        for enc in old_encodings:
+            channel = enc['channel']
+            del enc['channel']
+            spec['encoding'][channel] = enc
+        return
 
 
-def populate_field_names(spec):
-    counts = {
-        'n': 1, 'o': 1, 'q': 1, 't': 1
-    }
+    @staticmethod
+    def populate_field_names(spec):
+        counts = {
+            'n': 1, 'o': 1, 'q': 1, 't': 1
+        }
 
-    encodings = spec['encodings']
-    for enc in encodings:
-        field_type = enc['type'][:1]
-        field_name = field_type + str(counts[field_type])
-        counts[field_type] += 1
+        encodings = spec['encodings']
+        for enc in encodings:
+            field_type = enc['type'][:1]
+            field_name = field_type + str(counts[field_type])
+            counts[field_type] += 1
 
-        enc['field'] = field_name
-
-
-def load_json(file_path):
-    with open(file_path) as data:
-        return json.load(data)
-
-
-if __name__ == '__main__':
-    main()
+            enc['field'] = field_name
---
+++ draco/generation/run.py
@@ -0,0 +1,66 @@
+import json
+import os
+import random
+import math
+import argparse
+from copy import deepcopy
+import logging
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.WARN)
+
+from draco.generation.generator import Generator
+
+INTERACTIONS_PATH = os.path.join(
+    os.path.dirname(__file__), 'interactions.json')
+DISTRIBUTIONS_PATH = os.path.join(
+    os.path.dirname(__file__), 'distributions.json')
+DEFINITIONS_PATH = os.path.join(os.path.dirname(__file__), 'definitions.json')
+DUMMY_SCHEMA_PATH = os.path.join(
+    os.path.dirname(__file__), 'dummy_schema.json')
+
+NUM_TRIES = 100
+MAX_DIMENSIONS = 4
+
+def main(args):
+    interactions = load_json(INTERACTIONS_PATH)
+    distributions = load_json(DISTRIBUTIONS_PATH)
+    definitions = load_json(DEFINITIONS_PATH)
+    dummy_schema = load_json(DUMMY_SCHEMA_PATH)
+
+    generator = Generator(distributions, definitions, dummy_schema)
+
+    chosen = str(args.interaction)
+    num_groups = int(args.groups)
+
+    for interaction in interactions:
+        specified = interaction['name'] == chosen or chosen == 'all'
+        if (interaction['include'] and specified):
+            groups = []
+            for d in range(1, MAX_DIMENSIONS + 1):
+                for group_num in range(num_groups):
+                    specs = generator.generate_interaction(interaction['props'], d)
+
+                    tries = 0
+                    while (len(specs) < 2 and tries < NUM_TRIES):
+                        specs = generator.generate_interaction(interaction['props'], d)
+                        tries += 1
+                    groups.append(specs)
+
+            with open(args.output_dir, 'w') as out:
+                json.dump(groups, out, indent=2)
+                print('wrote {0}'.format(args.output_dir))
+
+
+def load_json(file_path):
+    with open(file_path) as data:
+        return json.load(data)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('interaction')
+    parser.add_argument('groups')
+    parser.add_argument('output_dir')
+    args = parser.parse_args()
+    main(args)
