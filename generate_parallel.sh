a0fd5d8797758dc26b284367631a3d92046dd3d5
haldenl <haldenl@cs.washington.edu>
2018-03-06 20:21:35 -0800

    modify generate_parallel.sh to reflect change in generation directory

--- generate_parallel.sh
+++ generate_parallel.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat draco/generation/interactions.json | jq -r '.[] .name' | parallel --eta python draco/generation/run.py --interaction {}
+cat draco/generation/define/interactions.json | jq -r '.[] .name' | parallel --eta python draco/generation/run.py --interaction {}
