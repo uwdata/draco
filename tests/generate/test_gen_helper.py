0620779e6e7ddaa92813372e46da72413b333dd2
haldenl <haldenl@cs.washington.edu>
2018-03-07 02:33:11 -0800

    add stack test that fails

--- tests/generate/test_gen_helper.py
+++ tests/generate/test_gen_helper.py
@@ -20,3 +20,27 @@ def test_is_valid():
         }
     })
     assert is_valid(Task(data, valid)) == True
+
+    data = Data(fields=[Field('n1', 'string'), Field('q1', 'number'), Field('q2', 'number')])
+
+    stack_color = Query.from_vegalite({
+        'mark': 'bar',
+        'encoding': {
+            'x': {
+                'type': 'nominal',
+                'field': 'n1',
+            },
+            'y': {
+                'type': 'quantitative',
+                'field': 'q1',
+                'stack': 'zero',
+                'aggregate': 'sum'
+            },
+            'color': {
+                'type': 'quantitative',
+                'field': 'q2'
+            }
+        }
+    })
+
+    assert is_valid(Task(data, stack_color), debug=True) == True
