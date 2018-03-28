53c62af034bf26ededddd31e472101da0b747127
haldenl <haldenl@cs.washington.edu>
2018-03-28 09:42:34 -0700

    timing

---
+++ benchmark/time_cql.js
@@ -0,0 +1,151 @@
+const cql = require('compassql');
+const fs = require('fs');
+
+const FIELD_NAMES = [
+  'Candidate_Identification',
+  'Candidate_Name',
+  'Incumbent_Challenger_Status',
+  'Party_Code',
+  'Party_Affiliation',
+  'Total_Receipts',
+  'Transfers_from_Authorized_Committees',
+  'Total_Disbursements',
+  'Transfers_to_Authorized_Committees',
+  'Beginning_Cash',
+  'Ending_Cash',
+  'Contributions_from_Candidate',
+  'Loans_from_Candidate',
+  'Other_Loans',
+  'Candidate_Loan_Repayments',
+  'Other_Loan_Repayments',
+  'Debts_Owed_By',
+  'Total_Individual_Contributions',
+  'Candidate_State',
+  'Candidate_District',
+  'Contributions_from_Other_Political_Committees',
+  'Contributions_from_Party_Committees',
+  'Coverage_End_Date',
+  'Refunds_to_Individuals',
+  'Refunds_to_Committees'
+]
+
+const ENCODING = {
+  "channel": "?",
+  "field": "?",
+  "type": "?"
+}
+
+const BASE_QUERY = {
+  "spec": {
+    "mark": "?"
+  },
+  "orderBy": "effectiveness",
+  "config": {
+    "autoAddCount": true
+  }
+}
+
+const NUM_FIELDS = [5, 10, 15, 20, 25];
+const NUM_ENCODINGS = [1, 2, 3, 4, 5];
+const NUM_TRIALS = 10;
+
+const INCLUDE = {
+  5: {
+    1: true,
+    2: true,
+    3: true,
+    4: true,
+    5: true,
+  },
+  10: {
+    1: true,
+    2: true,
+    3: true,
+    4: true,
+    5: false
+  },
+  15: {
+    1: true,
+    2: true,
+    3: true,
+    4: false,
+    5: false,
+  },
+  20: {
+    1: true,
+    2: true,
+    3: true,
+    4: false,
+    5: false,
+  },
+  25: {
+    1: true,
+    2: true,
+    3: true,
+    4: false,
+    5: false,
+  }
+};
+
+function main() {
+  // warmup
+  console.log('warming up...');
+  run_set()
+
+  // actual
+  const results = []
+  run_set(results);
+
+  fs.writeFileSync('cql_runtimes.json', JSON.stringify(results, null, 2), 'utf-8');
+}
+
+function run_set(results = null) {
+  for (nfields of NUM_FIELDS) {
+    const data = JSON.parse(fs.readFileSync('../data/weball26.json'));
+
+    for (const datum of data) {
+      for (let i = FIELD_NAMES.length - 1; i >= nfields; i--) {
+        delete datum[FIELD_NAMES[i]];
+      }
+    }
+
+    const schema =  cql.schema.build(data);
+
+    for (nencodings of NUM_ENCODINGS) {
+      if (INCLUDE[nfields][nencodings]) {
+        encodings = [];
+        for (let i = 0; i < nencodings; i++) {
+          const enc = Object.assign({}, ENCODING);
+          encodings.push(enc);
+        }
+
+        const query = Object.assign({}, BASE_QUERY);
+        query['spec']['encodings'] = encodings;
+
+        let total_time = 0;
+        for (let i = 0; i < NUM_TRIALS; i++) {
+          const startTime = new Date().getTime();
+          const recommendation =  cql.recommend(query, schema);
+          const endTime = new Date().getTime();
+
+          total_time += endTime - startTime;
+        }
+
+        const avg_time = total_time * 1.0 / NUM_TRIALS / 1000;
+
+        if (results !== null) {
+          results.push({
+            'fields': nfields,
+            'encodings': nencodings,
+            'runtime': avg_time,
+            'system': 'cql'
+          });
+
+          console.log('fields=' + nfields + ' encodings=' + nencodings + ' query time: ' + avg_time + 's');
+        }
+      }
+    }
+  }
+}
+
+main();
---
+++ benchmark/time_draco.py
@@ -0,0 +1,106 @@
+import subprocess
+import datetime
+import os
+import json
+from draco.run import run
+from draco.spec import Query, Task
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+DATA_FIELD_TYPES = ['string', 'string', 'string', 'number', 'string',
+                    'number', 'string', 'number', 'number', 'number',
+                    'number', 'number', 'number', 'string', 'number',
+                    'number', 'number', 'number', 'string', 'number',
+                    'string', 'number', 'datetime', 'number', 'string']
+
+NUM_FIELDS = [5, 10, 15, 20, 25]
+NUM_ENCODINGS = [1, 2, 3, 4, 5]
+NUM_TRIALS = 10
+
+CLINGO_PREFIX = 'clingo asp/_all.lp '
+CLINGO_OPTIONS = '--quiet=1 --warn=no-atom-undefined -c max_extra_encs=0'
+
+DRACO_LP_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../asp'))
+DRACO_LP = ['define.lp', 'generate.lp', 'hard.lp', 'soft.lp', 'weights.lp', 'assign_weights.lp', 'optimize.lp', 'output.lp']
+
+def main():
+    # warmup
+    logger.info('warming up...')
+    run_set()
+
+    # actual
+    results = []
+    run_set(results)
+
+    with open('draco_runtimes.json', 'w') as out:
+        json.dump(results, out, indent=2)
+
+def run_set(results = None):
+    for nfields in NUM_FIELDS:
+        for nencodings in NUM_ENCODINGS:
+            query = generate_asp_query(nfields, nencodings)
+
+            total_time = 0
+            for _ in range(NUM_TRIALS):
+                asp_query = generate_asp_query(nfields, nencodings)
+                total_time += run(asp_query)
+
+            avg_time = total_time / NUM_TRIALS
+
+            if (results is not None):
+                results.append({
+                    'fields': nfields,
+                    'encodings': nencodings,
+                    'runtime': avg_time,
+                    'system': 'draco'
+                })
+
+                logger.info('running fields={0} encodings={1} query time: {2}'.format(nfields, nencodings, avg_time))
+
+def run(asp_query):
+    # default args
+    options = ['--outf=2', '--quiet=1,2,2', '--warn=no-atom-undefined']
+
+    cmd = ['clingo'] + options
+
+    proc = subprocess.Popen(
+        args=cmd,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+
+    file_names = [os.path.join(DRACO_LP_DIR, f) for f in DRACO_LP]
+    asp_program = b'\n'.join(map(load_file, file_names)) + asp_query.encode('utf8')
+
+    start = datetime.datetime.now()
+    stdout, stderr = proc.communicate(asp_program)
+    end = datetime.datetime.now()
+
+    delta = end - start
+    return delta.total_seconds()
+
+def load_file(path):
+    with open(path) as f:
+        content = f.read().encode('utf8')
+        return content
+
+def generate_asp_query(nfields, nencodings):
+    query_string = ''
+
+    for i in range(nfields):
+        field_name = chr(ord('a') + i)
+        field_def = 'fieldtype({0},{1}).\n'.format(field_name, DATA_FIELD_TYPES[i])
+        query_string += field_def
+
+    for i in range(nencodings):
+        encoding_name = 'e{0}'.format(i)
+        encoding_def = 'encoding({0}).\n'.format(encoding_name)
+        query_string += encoding_def
+
+    return query_string
+
+
+if __name__ == '__main__':
+    main()
