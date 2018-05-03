e90840b3f1fad8d34e9673ed0510d919c2ba647a
haldenl <haldenl@cs.washington.edu>
2018-05-03 03:35:29 -0700

    add all.js (draco rules for web)

---
+++ draco-web/concat_lp.sh
@@ -0,0 +1,5 @@
+# usage: ./concat_lp.sh srcdir destdir
+
+all=$(cat $1/define.lp $1/generate.lp $1/hard.lp $1/soft.lp $1/weights.lp $1/assign_weights.lp $1/optimize.lp $1/output.lp | sed -e s/\`/\'/g)
+
+echo "const constraints = \`${all}\`;" > $2/all.js
