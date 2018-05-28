849bf9dbb319206d1251bb0a4dfc94685efd20bb
Zening Qu <quzening@gmail.com>
2018-05-28 13:59:22 -0700

    Hard-code data (cars.json) and schema

--- draco-web/src/spec.ts
+++ draco-web/src/spec.ts
@@ -1,10 +1,12 @@
 import {TopLevelSpec} from 'vega-lite';
 
 export default function asp2vl(asp: any): TopLevelSpec {
-    // TODO
+    // TODO: query.to_vega_lite: mark, encoding
+
     return {
+        '$schema': 'https://vega.github.io/schema/vega-lite/v2.0.json',
+        data: {url: 'data/cars.json'},
         mark: 'bar',
-        data: {values: []},
         encoding: {}
     }
 }
