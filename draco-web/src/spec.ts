ced461e833262d0d685c36dedaa699c65bdad28d
Zening Qu <quzening@gmail.com>
2018-05-28 15:31:26 -0700

    Translate one vega-lite spec for each witness

--- draco-web/src/spec.ts
+++ draco-web/src/spec.ts
@@ -1,12 +1,33 @@
 import {TopLevelSpec} from 'vega-lite';
 
-export default function asp2vl(asp: any): TopLevelSpec {
-    // TODO: query.to_vega_lite: mark, encoding
+export function asp2vl(asp: any): TopLevelSpec[] {
+    let specs: TopLevelSpec[] = [];
 
-    return {
-        '$schema': 'https://vega.github.io/schema/vega-lite/v2.0.json',
-        data: {url: 'data/cars.json'},
-        mark: 'bar',
-        encoding: {}
+    const witnesses = getWitnesses(asp);
+    if (witnesses) {
+        specs = witnesses.map((witness: any) => {
+            return {
+                '$schema': 'https://vega.github.io/schema/vega-lite/v2.0.json',
+                data: {url: 'data/cars.json'},
+                mark: 'bar',
+                encoding: {}
+            };
+        });
+    }
+    // console.log('specs', specs);
+    return specs;
+}
+
+/** 
+ * Get the array of witnesses from clingo output. 
+ * Return undefined if no witnesses were found. 
+ */
+function getWitnesses(asp: any) {
+    const result = JSON.parse(asp);
+
+    if(result.Call && result.Call.length > 0) {
+        return result.Call[0].Witnesses;
+    } else {
+        return undefined;
     }
 }
