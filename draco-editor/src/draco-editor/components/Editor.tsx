98d8532891541808092f209f95081a0dcb351015
Dominik Moritz <domoritz@gmail.com>
2018-05-27 20:24:46 -0700

    Add status panel

--- draco-editor/src/draco-editor/components/Editor.tsx
+++ draco-editor/src/draco-editor/components/Editor.tsx
@@ -1,15 +1,14 @@
-import Draco from 'draco-vis';
-import * as React from 'react';
-import MonacoEditor from 'react-monaco-editor';
-import SplitPane from 'react-split-pane';
+import Draco from "draco-vis";
+import * as React from "react";
+import MonacoEditor from "react-monaco-editor";
+import SplitPane from "react-split-pane";
 
-import '../styles/Editor.css';
-import '../styles/Resizer.css';
-
+import "../styles/Editor.css";
+import "../styles/Resizer.css";
 
 interface State {
-  code: string
-};
+  code: string;
+}
 
 class Editor extends React.Component<any, State> {
   draco: Draco;
@@ -17,10 +16,10 @@ class Editor extends React.Component<any, State> {
   public constructor(props: any) {
     super(props);
     this.state = {
-      code: ''
-    }
+      code: ""
+    };
 
-    this.draco = new Draco('dist');
+    this.draco = new Draco("dist");
 
     this.editorDidMount = this.editorDidMount.bind(this);
     this.handleEditorChange = this.handleEditorChange.bind(this);
@@ -28,7 +27,7 @@ class Editor extends React.Component<any, State> {
 
   public componentDidMount() {
     this.draco.init().then(() => {
-      console.log('initialized draco!');
+      console.log("initialized draco!");
     });
   }
 
@@ -40,18 +39,22 @@ class Editor extends React.Component<any, State> {
     return (
       <div className="Editor">
         <SplitPane split="vertical" defaultSize="40%" minSize={400}>
+          <div className="input-pane">
             <MonacoEditor
               options={{
                 automaticLayout: true,
-                cursorBlinking: 'smooth',
-                wordWrap: 'on',
-                wrappingIndent: 'same'
+                cursorBlinking: "smooth",
+                wordWrap: "on",
+                wrappingIndent: "same"
               }}
               language={null}
               value={this.state.code}
               editorDidMount={this.editorDidMount}
-              onChange={this.handleEditorChange}/>
-            <div>cool</div>
+              onChange={this.handleEditorChange}
+            />
+            <div className="status">Status</div>
+          </div>
+          <div className="recommendations">Output</div>
         </SplitPane>
       </div>
     );
