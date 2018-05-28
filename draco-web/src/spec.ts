8dae19759a4f7629798340349b67ae477cc89ceb
Zening Qu <quzening@gmail.com>
2018-05-28 16:33:49 -0700

    Translate mark

--- draco-web/src/spec.ts
+++ draco-web/src/spec.ts
@@ -6,15 +6,18 @@ export function asp2vl(asp: any): TopLevelSpec[] {
     const witnesses = getWitnesses(asp);
     if (witnesses) {
         specs = witnesses.map((witness: any) => {
+            // filter out soft constraint violations because we only need spec
+            const specValues = witness.Value.filter((s: string) => !s.startsWith('violation'));
+    
             return {
                 '$schema': 'https://vega.github.io/schema/vega-lite/v2.0.json',
                 data: {url: 'data/cars.json'},
-                mark: 'bar',
+                mark: parseMark(specValues),
                 encoding: {}
             };
         });
     }
-    // console.log('specs', specs);
+    console.log('specs', specs);
     return specs;
 }
 
@@ -30,4 +33,15 @@ function getWitnesses(asp: any) {
     } else {
         return undefined;
     }
+}
+
+/**
+ * Parse clingo output to get mark for vegalite spec
+ */
+function parseMark(values: string[]) {
+    const markValue = values.find((s: string) => s.startsWith('mark'));
+    if (markValue) {
+        return markValue.replace('mark(', '').replace(')', '');
+    }
+    return undefined;
 }
