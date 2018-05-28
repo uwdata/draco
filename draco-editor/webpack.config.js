dcdc1fb990936f219e87cf37fcfa78ca035080cd
haldenl <haldenl@cs.washington.edu>
2018-05-28 01:34:10 -0700

    switch to blues

--- draco-editor/webpack.config.js
+++ draco-editor/webpack.config.js
@@ -25,6 +25,12 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
       }
     ]
   },
