dc1574dfadfdc466bae751782d128eb6be6d5901
Dominik Moritz <domoritz@gmail.com>
2018-05-28 23:21:03 -0700

    Add test

--- draco-web/test/spec.test.ts
+++ draco-web/test/spec.test.ts
@@ -1,11 +1,21 @@
-import {asp2vl} from '../src/spec';
+import { asp2vl } from '../src/spec';
 
 test('parses results correctly', () => {
-  expect(asp2vl(['mark(bar)'])).toBe({
+  expect(asp2vl([
+    'mark(bar)',
+    'encoding(e0)',
+    'channel(e0,x)',
+    'field(e0,foo)',
+    'type(e0,ordinal)',
+    'encoding(e1)',
+    'channel(e1,y)',
+    'aggregate(e1,count)',
+    'type(e1,ordinal)',
+  ])).toBe({
     mark: 'bar',
     encoding: {
-      x: {field: 'foo', type: 'ordinal'},
-      y: {aggregate: 'count', type: 'quantitative'}
-    }
+      x: { field: 'foo', type: 'ordinal' },
+      y: { aggregate: 'count', type: 'quantitative' },
+    },
   });
 });
