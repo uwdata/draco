d5719ae71f99e3dc7ee0f01ebbdb23f5cac71f8d
Dominik Moritz <domoritz@gmail.com>
2018-05-28 23:37:17 -0700

    Fix test

--- draco-web/test/spec.test.ts
+++ draco-web/test/spec.test.ts
@@ -3,15 +3,19 @@ import { asp2vl } from '../src/spec';
 test('parses results correctly', () => {
   expect(asp2vl([
     'mark(bar)',
+
     'encoding(e0)',
     'channel(e0,x)',
     'field(e0,foo)',
     'type(e0,ordinal)',
+
     'encoding(e1)',
     'channel(e1,y)',
     'aggregate(e1,count)',
-    'type(e1,ordinal)',
+    'type(e1,quantitative)',
   ])).toBe({
+    $schema: 'https://vega.github.io/schema/vega-lite/v2.json',
+    data: {url: 'data/cars.json'},
     mark: 'bar',
     encoding: {
       x: { field: 'foo', type: 'ordinal' },
