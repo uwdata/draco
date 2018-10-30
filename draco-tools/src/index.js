94609628226b9e1053716d6f6c1274789f9743f1
Halden Lin <halden.lin@gmail.com>
2018-10-30 16:47:26 -0700

    move draco-tools to new repo

--- draco-tools/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'index/js/components/App';
-import registerServiceWorker from 'index/js/utilities/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
--- draco-tools/src/shared/js/utilities/util.js
+++
@@ -1,9 +0,0 @@
-/**
- * Returns true iff a and be are equal (deep equality).
- *
- * @param {Object} a
- * @param {Object} b
- */
-export function equals(a, b) {
-  return JSON.stringify(a) === JSON.stringify(b);
-}
