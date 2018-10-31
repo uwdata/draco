8c9a715f7fce875d543c4b064a8b0c6f3c006814
Dominik Moritz <domoritz@gmail.com>
2018-10-30 23:33:39 -0700

    Split tests

---
+++ js/test/asp2vl.test.ts
@@ -0,0 +1,28 @@
+import { asp2vl } from "../src";
+
+test('parses results correctly', () => {
+  expect(
+    asp2vl([
+      'mark(bar)',
+
+      'encoding(e0)',
+      'channel(e0,x)',
+      'field(e0,"foo")',
+      'type(e0,ordinal)',
+
+      'encoding(e1)',
+      'channel(e1,y)',
+      'aggregate(e1,count)',
+      'type(e1,quantitative)',
+      'zero(e1)',
+    ])
+  ).toEqual({
+    $schema: 'https://vega.github.io/schema/vega-lite/v3.json',
+    data: { url: 'data/cars.json' },
+    mark: 'bar',
+    encoding: {
+      x: { field: 'foo', type: 'ordinal' },
+      y: { aggregate: 'count', type: 'quantitative', scale: { zero: true } },
+    },
+  });
+});
---
+++ js/test/vl2asp.test.ts
@@ -0,0 +1,31 @@
+import { vl2asp } from "../src";
+
+test('generates correct asp', () => {
+  expect(
+    vl2asp({
+      $schema: 'https://vega.github.io/schema/vega-lite/v3.json',
+      data: { url: 'data/cars.json' },
+      mark: 'bar',
+      encoding: {
+        x: { field: 'foo', type: 'ordinal' },
+        y: { aggregate: 'count', type: 'quantitative', scale: { zero: true } },
+      },
+    }).sort()
+  ).toEqual(
+    [
+      'data("data/cars.json")',
+      'mark(bar)',
+
+      'encoding(e0)',
+      'channel(e0,x)',
+      'field(e0,"foo")',
+      'type(e0,ordinal)',
+
+      'encoding(e1)',
+      'channel(e1,y)',
+      'aggregate(e1,count)',
+      'type(e1,quantitative)',
+      'zero(e1)',
+    ].sort()
+  );
+});
