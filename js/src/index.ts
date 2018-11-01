c28525f01e109774a9ac70489ca15b3b5a07f2dd
Dominik Moritz <domoritz@gmail.com>
2018-10-31 17:18:07 -0700

    Export constraints together

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,5 +1,5 @@
-export * from './constraints';
 import asp2vl from './asp2vl';
+import * as constraints from './constraints';
 import vl2asp from './vl2asp';
 
-export { vl2asp, asp2vl };
+export { vl2asp, asp2vl, constraints };
