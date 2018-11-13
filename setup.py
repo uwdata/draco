d79a4b758ac9bf569c0ed26957826b150c3d77c6
Dominik Moritz <domoritz@gmail.com>
2018-11-12 18:10:30 -0800

    Use black for setup

--- setup.py
+++ setup.py
@@ -9,13 +9,14 @@ from setuptools import Command, setup
 from draco import __version__
 
 this_dir = abspath(dirname(__file__))
-with open(join(this_dir, 'README.md'), encoding='utf-8') as file:
+with open(join(this_dir, "README.md"), encoding="utf-8") as file:
     long_description = file.read()
 
 
 class RunTests(Command):
-    '''Run all tests.'''
-    description = 'run tests'
+    """Run all tests."""
+
+    description = "run tests"
     user_options = []
 
     def initialize_options(self):
@@ -25,44 +26,41 @@ class RunTests(Command):
         pass
 
     def run(self):
-        '''Run all tests!'''
+        """Run all tests!"""
 
-        print('=> Running Ansunit Tests:')
+        print("=> Running Ansunit Tests:")
 
-        errno_ansunit = call(['ansunit', 'asp/tests.yaml', '-v'])
+        errno_ansunit = call(["ansunit", "asp/tests.yaml", "-v"])
 
-        print('=> Running JS Tests:')
+        print("=> Running JS Tests:")
 
-        errno_js = call(['yarn', '--cwd', 'js', 'test'])
+        errno_js = call(["yarn", "--cwd", "js", "test"])
 
-        print('\n\n=> Running Python Tests:')
-        errno_pytest = call(['pytest', 'tests', '--cov=draco', '--cov-report=term-missing'])
+        print("\n\n=> Running Python Tests:")
+        errno_pytest = call(
+            ["pytest", "tests", "--cov=draco", "--cov-report=term-missing"]
+        )
 
-        print('\n\n=> Running MyPy:')
-        errno_mypy = call(['mypy', 'draco', 'tests', '--ignore-missing-imports'])
+        print("\n\n=> Running MyPy:")
+        errno_mypy = call(["mypy", "draco", "tests", "--ignore-missing-imports"])
 
-        print('\n\n=> Running Black:')
-        errno_mypy = call(['black', '--check', 'draco', 'tests'])
+        print("\n\n=> Running Black:")
+        errno_mypy = call(["black", "--check", "."])
 
         raise SystemExit(errno_ansunit + errno_js + errno_pytest + errno_mypy)
 
 
-setup(name='draco',
-      version=__version__,
-      description='Visualization recommendation using constraints',
-      long_description=long_description,
-      author='Dominik Moritz, Chenglong Wang',
-      author_email='domoritz@cs.washington.edu, clwang@cs.washington.edu',
-      license='BSD-3',
-      url='https://github.com/uwdata/draco',
-      packages = ['draco'],
-      entry_points = {
-        'console_scripts': [
-            'draco=draco.cli:main',
-        ],
-      },
-      extras_require = {
-        'test': ['coverage', 'pytest', 'pytest-cov'],
-      },
-      cmdclass = {'test': RunTests},
-     )
+setup(
+    name="draco",
+    version=__version__,
+    description="Visualization recommendation using constraints",
+    long_description=long_description,
+    author="Dominik Moritz, Chenglong Wang",
+    author_email="domoritz@cs.washington.edu, clwang@cs.washington.edu",
+    license="BSD-3",
+    url="https://github.com/uwdata/draco",
+    packages=["draco"],
+    entry_points={"console_scripts": ["draco=draco.cli:main"]},
+    extras_require={"test": ["coverage", "pytest", "pytest-cov"]},
+    cmdclass={"test": RunTests},
+)
