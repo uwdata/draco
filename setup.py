d5503f99e02e55f601596af8148783317ad14d8a
Halden Lin <halden.lin@gmail.com>
2018-12-02 12:05:37 -0800

    reformat

--- setup.py
+++ setup.py
@@ -54,8 +54,6 @@ class RunTests(Command):
         print("\n\n=> Running Black:")
         errno_mypy = call(["black", "--check", "."])
 
-
-
         raise SystemExit(errno_ansunit + errno_js + errno_pytest + errno_mypy)
 
 
