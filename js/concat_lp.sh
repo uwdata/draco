24b1b5d1c794357b7f65c0b765b3bed6f7b76de2
Halden Lin <halden.lin@gmail.com>
2019-01-22 23:58:04 -0800

    correct newlines in concat.sh

--- js/concat_lp.sh
+++ js/concat_lp.sh
@@ -14,12 +14,18 @@ declare -a files=("topk-lua"
 newline=$'\n\n'
 output="// GENERATED WITH concat_lp.sh. DO NOT MODIFY.${newline}"
 
+i=0
 for file in "${files[@]}"
 do
   path="${1}/${file}.lp"
   lp=$(cat $path | sed -e s/\`/\'/g)
   const=$(echo $file | tr a-z A-Z | tr \- _)
-  output+="${newline}export const ${const}: string = \`${lp}\`;"
+  if [ "$i" -ne 0 ]
+  then
+    output+="${newline}"
+  fi
+  output+="export const ${const}: string = \`${lp}\`;"
+  let "i++"
 done
 
 echo "$output" > $2/constraints.ts
