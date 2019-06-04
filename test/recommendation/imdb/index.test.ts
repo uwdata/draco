aee47a2f27f4885dd1dd264e8758caeb982b6e25
Halden Lin <halden.lin@gmail.com>
2019-06-03 17:09:27 -0700

    Refactor tests, add additional constraints.

---
+++ test/recommendation/imdb/index.test.ts
@@ -0,0 +1,26 @@
+import { readdirSync } from "fs";
+import "jest-extended";
+import path from "path";
+import { Data, Draco, Result } from "../../../src";
+
+describe("IMDB", () => {
+  readdirSync(path.resolve(__dirname, "./")).forEach(file => {
+    const [name, ext] = file.split(".");
+    if (ext === "lp") {
+      test(name, () => {
+        const input = path.resolve(__dirname, file);
+        const output = `${name}.json`;
+        const expected = require(path.resolve(__dirname, output));
+
+        const result = Draco.run(DATA.asp, null, [input]);
+        const specs = Result.getBestVegaLiteSpecDictionary(result);
+
+        expect(specs).toBeOneOf(expected);
+      });
+    }
+  });
+});
+
+const MOVIES = require(path.resolve(__dirname, "../../../data/movies.json"));
+
+const DATA = Data.fromArray(MOVIES);
