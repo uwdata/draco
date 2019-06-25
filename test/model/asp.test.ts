2796381fd41ef4442649e2c183bfc664d19d5b82
Halden Lin <halden.lin@gmail.com>
2019-06-24 18:58:58 -0700

    Refactor data lp.

--- test/model/asp.test.ts
+++ test/model/asp.test.ts
@@ -1,13 +1,14 @@
-const { spawnSync } = require('child_process');
-const path = require('path');
+const { spawnSync } = require("child_process");
+const path = require("path");
 
-describe('Check ASP warnings', () => {
-  test('No warnings', () => {
-    const program = path.resolve(__dirname, '../../model/program/default.lp');
-    const data = path.resolve(__dirname,'./data.lp');
+describe("Check ASP warnings", () => {
+  test("No warnings", () => {
+    const program = path.resolve(__dirname, "../../model/program/check.lp");
 
-    const output = spawnSync('clingo', ['-q', '--outf=2', data, program], { encoding: 'utf8' });
+    const output = spawnSync("clingo", ["-q", "--outf=2", program], {
+      encoding: "utf8"
+    });
 
-    expect(output.stderr).toEqual('');
+    expect(output.stderr).toEqual("");
   });
 });
