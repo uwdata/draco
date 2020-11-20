b52e58ff611ea1b28ccad3128fa94190f7983829
Harshil Rastogi <harshil9968@gmail.com>
2020-11-20 10:53:23 +0530

    style: fix black errors (#101)

--- draco/helper.py
+++ draco/helper.py
@@ -8,11 +8,11 @@ from draco.run import run_clingo
 
 
 def is_valid(draco_query: List[str], debug=False) -> bool:
-    """ Check a task.
-        Args:
-            draco_query: a list of facts
-        Returns:
-            whether the task is valid
+    """Check a task.
+    Args:
+        draco_query: a list of facts
+    Returns:
+        whether the task is valid
     """
     _, stdout = run_clingo(
         draco_query,
@@ -25,21 +25,21 @@ def is_valid(draco_query: List[str], debug=False) -> bool:
 
 
 def data_to_asp(data: List) -> List[str]:
-    """ Reads the data array and generates the ASP definition.
-        Args:
-            file: the data as a list of objects
-        Returns:
-            the asp definition.
+    """Reads the data array and generates the ASP definition.
+    Args:
+       file: the data as a list of objects
+    Returns:
+       the asp definition.
     """
     return schema2asp(data2schema(data))
 
 
 def read_data_to_asp(file: str) -> List[str]:
-    """ Reads the given JSON file and generates the ASP definition.
-        Args:
-            file: the json data file
-        Returns:
-            the asp definition.
+    """Reads the given JSON file and generates the ASP definition.
+    Args:
+       file: the json data file
+    Returns:
+       the asp definition.
     """
     if file.endswith(".json"):
         with open(file) as f:
